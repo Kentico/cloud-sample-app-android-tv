50bb5ca6af2d72c527917556506e75db5469293f
JurajB <jurajb@kentico.com>
2018-03-25 16:45:14 +0200

    Add MainActivity

---
+++ app/src/main/java/kentico/kentico_android_tv_app/MainActivity.java
@@ -0,0 +1,13 @@
+package kentico.kentico_android_tv_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/kentico/kentico_android_tv_app/MainFragment.java
@@ -0,0 +1,22 @@
+package kentico.kentico_android_tv_app;
+
+import android.os.Bundle;
+import android.support.v17.leanback.app.BrowseFragment;
+
+/**
+ * Created by Juraj on 25.03.2018.
+ */
+
+public class MainFragment extends BrowseFragment {
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
