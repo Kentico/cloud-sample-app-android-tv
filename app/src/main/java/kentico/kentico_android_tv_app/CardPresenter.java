a11b9861e91a09a96dd05fff273d0471293c430b
JurajB <jurajb@kentico.com>
2018-03-27 22:52:03 +0200

    Try to implement MainActivity

---
+++ app/src/main/java/kentico/kentico_android_tv_app/CardPresenter.java
@@ -0,0 +1,90 @@
+package kentico.kentico_android_tv_app;
+
+/**
+ * Created by Juraj on 27.03.2018.
+ */
+
+import android.graphics.drawable.Drawable;
+import android.support.v17.leanback.widget.ImageCardView;
+import android.support.v17.leanback.widget.Presenter;
+import android.util.Log;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+
+import kentico.kentico_android_tv_app.data.models.Article;
+
+/*
+ * A CardPresenter is used to generate Views and bind Objects to them on demand.
+ * It contains an Image CardView
+ */
+public class CardPresenter extends Presenter {
+    private static final String TAG = "CardPresenter";
+
+    private static final int CARD_WIDTH = 313;
+    private static final int CARD_HEIGHT = 176;
+    private static int sSelectedBackgroundColor;
+    private static int sDefaultBackgroundColor;
+    private Drawable mDefaultCardImage;
+
+    private static void updateCardBackgroundColor(ImageCardView view, boolean selected) {
+        int color = selected ? sSelectedBackgroundColor : sDefaultBackgroundColor;
+        // Both background colors should be set because the view's background is temporarily visible
+        // during animations.
+        view.setBackgroundColor(color);
+        view.findViewById(R.id.info_field).setBackgroundColor(color);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent) {
+        Log.d(TAG, "onCreateViewHolder");
+
+        sDefaultBackgroundColor = parent.getResources().getColor(R.color.default_background);
+        sSelectedBackgroundColor = parent.getResources().getColor(R.color.selected_background);
+        /*
+         * This template uses a default image in res/drawable, but the general case for Android TV
+         * will require your resources in xhdpi. For more information, see
+         * https://developer.android.com/training/tv/start/layouts.html#density-resources
+         */
+
+        ImageCardView cardView = new ImageCardView(parent.getContext()) {
+            @Override
+            public void setSelected(boolean selected) {
+                updateCardBackgroundColor(this, selected);
+                super.setSelected(selected);
+            }
+        };
+
+        cardView.setFocusable(true);
+        cardView.setFocusableInTouchMode(true);
+        updateCardBackgroundColor(cardView, false);
+        return new ViewHolder(cardView);
+    }
+
+    @Override
+    public void onBindViewHolder(Presenter.ViewHolder viewHolder, Object item) {
+        Article article = (Article) item;
+        ImageCardView cardView = (ImageCardView) viewHolder.view;
+
+        Log.d(TAG, "onBindViewHolder");
+        if (article != null) {
+            cardView.setTitleText(article.getTitle());
+            cardView.setContentText(article.getSummary());
+            cardView.setMainImageDimensions(CARD_WIDTH, CARD_HEIGHT);
+            Glide.with(viewHolder.view.getContext())
+                    .load(article.teaserImage)
+                    .centerCrop()
+                    .into(cardView.getMainImageView());
+        }
+    }
+
+    @Override
+    public void onUnbindViewHolder(Presenter.ViewHolder viewHolder) {
+        Log.d(TAG, "onUnbindViewHolder");
+        ImageCardView cardView = (ImageCardView) viewHolder.view;
+        // Remove references to images so that the garbage collector can free up memory
+        cardView.setBadgeImage(null);
+        cardView.setMainImage(null);
+    }
+}
+
--- app/src/main/java/kentico/kentico_android_tv_app/MainFragment.java
+++ app/src/main/java/kentico/kentico_android_tv_app/MainFragment.java
@@ -1,26 +1,118 @@
 package kentico.kentico_android_tv_app;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v17.leanback.app.BackgroundManager;
 import android.support.v17.leanback.app.BrowseFragment;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
+import android.support.v17.leanback.widget.HeaderItem;
+import android.support.v17.leanback.widget.ListRow;
+import android.support.v17.leanback.widget.ListRowPresenter;
+import android.util.DisplayMetrics;
+
 import com.kenticocloud.delivery_android.DeliveryAndroidService;
+import com.kenticocloud.delivery_core.query.item.MultipleItemQuery;
 import com.kenticocloud.delivery_core.services.IDeliveryService;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Timer;
+
+import kentico.kentico_android_tv_app.config.AppConfig;
+import kentico.kentico_android_tv_app.data.models.Article;
+import kentico.kentico_android_tv_app.data.source.articles.ArticlesRepository;
+import kentico.kentico_android_tv_app.injection.Injection;
 
 /**
  * Created by Juraj on 25.03.2018.
  */
 
 public class MainFragment extends BrowseFragment {
+    private static final String TAG = "MainFragment";
+
+    private static final int BACKGROUND_UPDATE_DELAY = 300;
+    private static final int GRID_ITEM_WIDTH = 200;
+    private static final int GRID_ITEM_HEIGHT = 200;
+    private static final int NUM_ROWS = 6;
+    private static final int NUM_COLS = 15;
+
+    private final Handler mHandler = new Handler();
+    private ArrayObjectAdapter mRowsAdapter;
+    private Drawable mDefaultBackground;
+    private DisplayMetrics mMetrics;
+    private Timer mBackgroundTimer;
+    private String mBackgroundUri;
+    private BackgroundManager mBackgroundManager;
+
+    private IDeliveryService deliveryService = Injection.provideDeliveryService();
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-//        IDeliveryService androidDeliveryService = new DeliveryAndroidService();
+        prepareBackgroundManager();
 
+        setupUIElements();
+
+        loadRows();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+    }
+
+    private void prepareBackgroundManager() {
+
+        mBackgroundManager = BackgroundManager.getInstance(getActivity());
+        mBackgroundManager.attach(getActivity().getWindow());
+        mDefaultBackground = getResources().getDrawable(R.drawable.default_background);
+        mMetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(mMetrics);
+    }
+
+    private void setupUIElements() {
+        setTitle(getString(R.string.browse_title));
+
+        setHeadersState(HEADERS_ENABLED);
+        setHeadersTransitionOnBackEnabled(true);
+
+
+        setBrandColor(getResources().getColor(R.color.fastlane_background));
+        setSearchAffordanceColor(getResources().getColor(R.color.search_opaque));
+    }
+
+    private void loadRows() {
+        MultipleItemQuery<Article> articlesQuery = deliveryService.items();
+
+        List<Article> list = articlesQuery.get().getItems();
+
+        mRowsAdapter = new ArrayObjectAdapter(new ListRowPresenter());
+        CardPresenter cardPresenter = new CardPresenter();
+
+        int i;
+        for (i = 0; i < list.size(); i++) {
+            if (i != 0) {
+                Collections.shuffle(list);
+            }
+            ArrayObjectAdapter listRowAdapter = new ArrayObjectAdapter(cardPresenter);
+            for (int j = 0; j < list.size(); j++) {
+                listRowAdapter.add(list.get(j % 5));
+            }
+            HeaderItem header = new HeaderItem(i, list.get(i).getTitle());
+            mRowsAdapter.add(new ListRow(header, listRowAdapter));
+        }
+
+//        HeaderItem gridHeader = new HeaderItem(i, "PREFERENCES");
+//
+//        GridItemPresenter mGridPresenter = new GridItemPresenter();
+//        ArrayObjectAdapter gridRowAdapter = new ArrayObjectAdapter(mGridPresenter);
+//        gridRowAdapter.add(getResources().getString(R.string.grid_view));
+//        gridRowAdapter.add(getString(R.string.error_fragment));
+//        gridRowAdapter.add(getResources().getString(R.string.personal_settings));
+//        mRowsAdapter.add(new ListRow(gridHeader, gridRowAdapter));
+
+        setAdapter(mRowsAdapter);
     }
 }
