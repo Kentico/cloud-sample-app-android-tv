4afd37f567ed39c37313fbcfa7b973f817537ac2
JurajB <jurajb@kentico.com>
2018-03-27 20:56:02 +0200

    Add Article data model

--- app/src/main/java/kentico/kentico_android_tv_app/MainFragment.java
+++ app/src/main/java/kentico/kentico_android_tv_app/MainFragment.java
@@ -2,6 +2,8 @@ package kentico.kentico_android_tv_app;
 
 import android.os.Bundle;
 import android.support.v17.leanback.app.BrowseFragment;
+import com.kenticocloud.delivery_android.DeliveryAndroidService;
+import com.kenticocloud.delivery_core.services.IDeliveryService;
 
 /**
  * Created by Juraj on 25.03.2018.
@@ -12,6 +14,8 @@ public class MainFragment extends BrowseFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+//        IDeliveryService androidDeliveryService = new DeliveryAndroidService();
 
     }
 
